57675f872a74b4cdd570c9d2f9f69307c7792a8f
Daniere Mathieu <mathieudaniere@orange.fr>
2021-10-13 10:07:08 +0000

    training routes and wiews

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" type="text/css" href="{{ URL::to('css/menu.css') }}">
+<header class="header_menu">
+  <nav class="nav_header_menu">
+    <img src="https://scan-trad.fr//content/b74f5ee9461495ba5ca4c72a7108a23904c27a05/1587233833.png" alt="" class="img_header_menu">
+
+    <ul class="ul_header_menu">
+      <li><a href="/">welcome</a> </li>
+      <li><a href="/test">test</a></li>
+      <li><a href="/jesais/test2">test2</a></li>
+    </ul>
+  </nav>
+</header>
